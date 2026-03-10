47c538c005cca5b6f7d79cd460fc9cf9ffb8f11e
philexlive <philex.live@outlook.com>
2026-03-10 16:50:28 +0000

    prep_logs.py minor changes.

--- prep_logs.py
+++ prep_logs.py
@@ -4,16 +4,14 @@ import pandas as pd
 from kagglehub import KaggleDatasetAdapter
 
 
-df: pd.DataFrame = kagglehub.dataset_load(
-    KaggleDatasetAdapter.PANDAS,
-    'philexfertich/personal-sleep-tracking-and-quality-logs',
-    "sleep-logs.csv",
-    pandas_kwargs={
-        'parse_dates': ['Sleep Time', 'Wake Time'],
-        'date_format': r'%Y-%m-%d %H:%M:%S',
-        'converters': {'Duration': pd.to_timedelta}
-    }
-)
-
-print(df)
-df.info()
+def get_sleep_data():
+    return kagglehub.dataset_load(
+        KaggleDatasetAdapter.PANDAS,
+        'philexfertich/personal-sleep-tracking-and-quality-logs',
+        "sleep-logs.csv",
+        pandas_kwargs={
+            'parse_dates': ['Sleep Time', 'Wake Time'],
+            'date_format': r'%Y-%m-%d %H:%M:%S',
+            'converters': {'Duration': pd.to_timedelta}
+        }
+    )
